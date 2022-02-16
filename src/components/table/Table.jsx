85d36103300adce22ef5b7ec547b3e813ec582d2
Sigfredo Guzman <sigfredo.guzman@leidos.com>
2022-02-16 12:38:11 -0500

    Deleted unused code as well as implemented data coming back from API into the data grid.

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -3,52 +3,14 @@ import { Box } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { stocksApi, useGetAllStocksQuery } from "../../api/api";
 
-// const { data, error, isLoading } = useGetAllStocksQuery();
-// const result = stocksApi.endpoints.getAllStocks.select();
-// const { data, status, error } = result;
-// const { data } = useGetAllStocksQuery();
+// Will auto update stock prices
 // useEffect(() => {}, [data]);
-// console.log("Data >>>>", data);
+
 const columns = [
-  { field: "id", headerName: "ID", width: 70 },
+  { field: "alphaID", headerName: "ID", width: 70 },
   { field: "companyName", headerName: "Company Name", width: 130 },
   { field: "value", headerName: "Price", width: 70 },
 ];
-
-// const rows = data.map;
-
-// const columns = [
-//   { field: "id", headerName: "ID", width: 70 },
-//   { field: "firstName", headerName: "First name", width: 130 },
-//   { field: "lastName", headerName: "Last name", width: 130 },
-//   {
-//     field: "age",
-//     headerName: "Age",
-//     type: "number",
-//     width: 90,
-//   },
-//   {
-//     field: "fullName",
-//     headerName: "Full name",
-//     description: "This column has a value getter and is not sortable.",
-//     sortable: false,
-//     width: 160,
-//     valueGetter: (params) =>
-//       `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-//   },
-// ];
-
-// const rows = [
-//   { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-//   { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-//   { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-//   { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-//   { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-//   { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-//   { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-//   { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-//   { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-// ];
 
 export default function Table() {
   const { data, error, isLoading } = useGetAllStocksQuery();
@@ -58,11 +20,11 @@ export default function Table() {
       {data ? (
         <DataGrid
           autoHeight
+          getRowId={(row) => row.alphaID}
           rows={data}
           columns={columns}
           pageSize={5}
           rowsPerPageOptions={[5]}
-          checkboxSelection
         />
       ) : (
         <p>Nothing found</p>
